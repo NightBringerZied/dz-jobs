e37d233c118bd0370569e39b2076e9e63f917520
NightBringerZied <benaliouchezied@gmail.com>
2024-12-17 20:13:09 +0100

    fix deploy

--- app/components/layout.js
+++ app/components/layout.js
@@ -1,23 +1,32 @@
 "use client"
-import { useEffect, useRef } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
-import 'locomotive-scroll/dist/locomotive-scroll.css';
 
+import { useEffect, useRef } from 'react';
+import dynamic from 'next/dynamic';
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+const LocomotiveScroll = dynamic(() => import('locomotive-scroll'), { ssr: false });
 
 export default function Layout({ children }) {
   const scrollRef = useRef(null);
 
   useEffect(() => {
-    const scroll = new LocomotiveScroll({
-      el: scrollRef.current,
-      smooth: true,
-    });
-    const onResize = () => scroll.update();
-    window.addEventListener('resize', onResize);
-    return () => {
-      scroll.destroy(); 
-      window.removeEventListener('resize', onResize);// Cleanup when the component unmounts
+    const initScroll = async () => {
+      const LocomotiveScroll = await import('locomotive-scroll'); 
+      if (scrollRef.current) {
+        const scroll = new LocomotiveScroll.default({
+          el: scrollRef.current,
+          smooth: true,
+        });
+
+        return () => {
+          scroll.destroy(); // Cleanup on unmount
+        };
+      }
     };
+
+    if (typeof window !== 'undefined') { // Ensure window object exists (client-side)
+      initScroll();
+    }
   }, []);
 
   return (
